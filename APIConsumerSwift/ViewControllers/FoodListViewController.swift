8a37566850c3364e294f5aea9a5a9b80fe25fb1d
Andrew Dolce <andrew@intrepid.io>
2016-01-05 21:27:58 -0500

    Populate table with test foods

--- APIConsumerSwift/ViewControllers/FoodListViewController.swift
+++ APIConsumerSwift/ViewControllers/FoodListViewController.swift
@@ -8,12 +8,56 @@
 
 import UIKit
 
-class FoodListViewController: UIViewController {
+class FoodListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var foods = [Food]()
+
+    private let foodCellReuseIdentifier = "foodCellReuseIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupTableView()
+
+        addTestFoods()
     }
+
+    private func setupTableView() {
+        tableView.tableFooterView = UIView()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    private func addTestFoods() {
+        foods.append(Food(identifier: "foobar1", name: "Hot Dog", isHealthy: false))
+        foods.append(Food(identifier: "foobar2", name: "Hamburger", isHealthy: false))
+        foods.append(Food(identifier: "foobar3", name: "Salad", isHealthy: true))
+        foods.append(Food(identifier: "foobar4", name: "Dank Grapes", isHealthy: true))
+        tableView.reloadData()
+    }
+
+    // MARK: - UITableViewDataSource
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let food = foods[indexPath.row]
+
+        let cell = tableView.dequeueReusableCellWithIdentifier(foodCellReuseIdentifier) ?? UITableViewCell(style: .Default, reuseIdentifier: foodCellReuseIdentifier)
+        cell.textLabel?.text = food.name
+        cell.textLabel?.textColor = food.isHealthy ? UIColor.greenColor() : UIColor.redColor()
+
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate
 
 }
