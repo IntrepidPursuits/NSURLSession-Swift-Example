c54351c984ffa000e7d43fe428bbb96949e1e32a
Andrew Dolce <andrew@intrepid.io>
2016-01-05 19:23:08 -0500

    Stub out Food model class

---
+++ APIConsumerSwift/Models/Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  APIConsumerSwift
+//
+//  Created by Andrew Dolce on 1/5/16.
+//  Copyright © 2016 Intrepid Pursuits. All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+    let identifier: String
+    let name: String
+    let isHealthy: Bool
+}
